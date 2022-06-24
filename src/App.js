cbe3eb4274d17df4179e298ade96b0c2d1c40b8f
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-06-24 15:59:14 -0400

    added HeaderCartButton component to the header, and the Meals component which uses MealsSummary and AvailableMeals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./components/L:ayout/Header";
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
