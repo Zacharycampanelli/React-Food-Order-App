cf7f23f1fe26c7e6f3011a1d48f055f469726af1
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2022-06-24 21:00:56 -0400

    added Meals Components and Input Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Header from "./components/L:ayout/Header";
+import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 
 function App() {
